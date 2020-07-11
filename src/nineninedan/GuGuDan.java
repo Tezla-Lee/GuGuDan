e418e8dc0cdf079987bbde0231ab54d49f71f644
Tezla-Lee <tezla.lee@gmail.com>
2020-07-12 01:11:16 +0900

    6Dan & 7Dan

---
+++ src/nineninedan/GuGuDan.java
@@ -0,0 +1,40 @@
+package nineninedan;
+
+public class GuGuDan {
+
+	public static void main(String[] args) {
+
+		// 2단 3단
+//		int dan;
+//		int count;
+//		
+//		for (dan = 2; dan <= 3; dan++) {
+//			
+//			for( count = 1; count <= 9; count++) {
+//				
+//				System.out.println(dan + " X " + count + " = " + count * dan);
+//			}
+//			
+//			System.out.println();
+//		}
+
+		// 6단
+		
+		
+		int i = 1;
+
+		while(i < 10) {		
+			System.out.println(6 * i);		
+			i++;
+		}
+		
+		// 7단
+		i = 1;
+		while(i < 10) {		
+			System.out.println(7 * i);		
+			i++;
+		}
+		
+	}
+
+}
