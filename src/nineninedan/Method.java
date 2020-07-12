9a625c3a4763c03d9d579d039d191e1187a779bc
Tezla-Lee <tezla.lee@gmail.com>
2020-07-12 12:58:59 +0900

    GuGuDan using Method

---
+++ src/nineninedan/Method.java
@@ -0,0 +1,26 @@
+package nineninedan;
+
+public class Method {
+
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for (int i= 0; i < 8; i++) {
+		int []result = calculate(i+2);
+			print(result);
+		}
+	}
+
+}
